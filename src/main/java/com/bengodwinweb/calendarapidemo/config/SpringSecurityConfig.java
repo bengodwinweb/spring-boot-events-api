78addd2e99d7e5043edd937c27fe69a0002507c6
Ben Godwin <ben.godwin1@gmail.com>
2020-04-05 13:01:29 -0400

    authorize users with JWT

--- src/main/java/com/bengodwinweb/calendarapidemo/config/SpringSecurityConfig.java
+++ src/main/java/com/bengodwinweb/calendarapidemo/config/SpringSecurityConfig.java
@@ -1,17 +1,21 @@
 package com.bengodwinweb.calendarapidemo.config;
 
 import com.bengodwinweb.calendarapidemo.Model.Roles;
+import com.bengodwinweb.calendarapidemo.services.JwtRequestFilter;
 import com.bengodwinweb.calendarapidemo.services.MongoUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableConfigurationProperties
@@ -21,23 +25,38 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private MongoUserDetailsService userDetailsService;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    @Autowired
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
+    @Autowired
+    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
     }
 
+    @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .authorizeRequests().antMatchers("/", "/users/register").permitAll()
+                .authorizeRequests().antMatchers("/", "/users/register", "/authenticate").permitAll()
                 .antMatchers("/users").hasRole(Roles.ADMIN.get())
                 .anyRequest().authenticated()
-                .and().httpBasic()
-                .and().sessionManagement().disable();
+                .and().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }

dc76e96dac91abd74e575ac9fb01eb635a8f055a
Ben Godwin <ben.godwin1@gmail.com>
2020-04-05 13:25:10 -0400

    respond with jwt on new user registration

--- src/main/java/com/bengodwinweb/calendarapidemo/controllers/RegistrationController.java
+++ src/main/java/com/bengodwinweb/calendarapidemo/controllers/RegistrationController.java
@@ -1,16 +1,19 @@
 package com.bengodwinweb.calendarapidemo.controllers;
 
+import com.bengodwinweb.calendarapidemo.Model.JwtResponse;
 import com.bengodwinweb.calendarapidemo.Model.User;
 import com.bengodwinweb.calendarapidemo.Model.UserDto;
 import com.bengodwinweb.calendarapidemo.exceptionHandling.EmailExistsException;
 import com.bengodwinweb.calendarapidemo.exceptionHandling.UserDtoValidationException;
+import com.bengodwinweb.calendarapidemo.services.JwtTokenService;
+import com.bengodwinweb.calendarapidemo.services.MongoUserDetailsService;
 import com.bengodwinweb.calendarapidemo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,6 +28,12 @@ public class RegistrationController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private MongoUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtTokenService jwtTokenService;
+
     @GetMapping("/users/register")
     @ResponseBody
     public String showRegister() {
@@ -32,25 +41,40 @@ public class RegistrationController {
         return "register page";
     }
 
+//    @PostMapping("/users/register")
+//    @PreAuthorize("permitAll()")
+//    @ResponseBody
+//    public User registerUserAccount(@RequestBody @Valid UserDto accountDto, BindingResult result, WebRequest request, Errors errors) throws EmailExistsException, UserDtoValidationException {
+//        User registered = new User();
+//        if (!result.hasErrors()) {
+//            registered = createUserAccount(accountDto, result);
+//        } else {
+//            System.out.println(errors);
+//            for (FieldError err : errors.getFieldErrors()) {
+//                System.out.println(err.getField());
+//                System.out.println(err.getDefaultMessage());
+//            }
+//            throw new UserDtoValidationException("Validation Error", errors);
+//        }
+//        if (registered == null) {
+//            result.rejectValue("email", "message.regError");
+//        }
+//        return registered;
+//    }
+
     @PostMapping("/users/register")
-    @PreAuthorize("permitAll()")
-    @ResponseBody
-    public User registerUserAccount(@RequestBody @Valid UserDto accountDto, BindingResult result, WebRequest request, Errors errors) throws EmailExistsException, UserDtoValidationException {
-        User registered = new User();
-        if (!result.hasErrors()) {
-            registered = createUserAccount(accountDto, result);
-        } else {
-            System.out.println(errors);
-            for (FieldError err : errors.getFieldErrors()) {
-                System.out.println(err.getField());
-                System.out.println(err.getDefaultMessage());
-            }
-            throw new UserDtoValidationException("Validation Error", errors);
-        }
-        if (registered == null) {
-            result.rejectValue("email", "message.regError");
-        }
-        return registered;
+    public ResponseEntity<?> registerNewUser(@RequestBody @Valid UserDto newUserDto, BindingResult result, WebRequest request, Errors errors) throws EmailExistsException, UserDtoValidationException {
+
+        if (result.hasErrors()) throw new UserDtoValidationException("Validation Error", errors);
+
+        User newUser = new User();
+        newUser = createUserAccount(newUserDto, result);
+
+        if (newUser == null) result.rejectValue("email", "message.regError");
+
+        final UserDetails newUserDetails = userDetailsService.loadUserByUsername(newUser.getEmail());
+        final String token = jwtTokenService.generateToken(newUserDetails);
+        return ResponseEntity.ok(new JwtResponse(token));
     }
 
     private User createUserAccount(UserDto accountDto, BindingResult result) throws EmailExistsException {
